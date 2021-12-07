d2bc3645bb1b35c2787fa70a40508a8df1233044
Theo <theo.hallenius@gmail.com>
2021-12-07 16:27:24 +0100

    rpub

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: 'https://theoxd.github.io/rdev-web',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    site: 'https://r-publishing.github.io/rpub-web',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true,         // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
